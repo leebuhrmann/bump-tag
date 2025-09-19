4a5a37cdea5d53c07f89e4de0ad0897cb96e6488
Lee Buhrmann <lee.buhrmann@gmail.com>
2025-09-18 20:59:54 -0500

    need to pass token to script silly.

--- pull-repos.ps1
+++ pull-repos.ps1
@@ -1,7 +1,12 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$token
+)
+
 $Uri = "https://api.github.com/users/leebuhrmann/repos"
 $Headers = @{
     "Accept" = "application/vnd.github+json"
-    "Authorization"="Bearer ${{ secrets.github_pat }}"
+    "Authorization"="Bearer $token"
     "X-GitHub-Api-Version"="2022-11-28"
 }
 $Response = Invoke-RestMethod -Uri $Uri -Headers $Headers -Method Get
